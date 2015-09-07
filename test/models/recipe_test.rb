428d5d54ddb8da63428d4e51bf618853aaa7622b
zacscodingclub <zbaston@gmail.com>
2015-09-07 20:05:58 +0000

    Created tables, models, and tests for app.  Tests setup to pass for now.

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,61 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  
+  def setup
+    @recipe = Recipe.new(
+      name: "pizza",
+      summary: "A modern take on an old classic",
+      description: "Lorem Ipsum dollar shank floral bacon tester"
+    )
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too long" do
+    @recipe.name = "a" * 101
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too short" do
+    @recipe.name = "aa"
+    assert_not @recipe.valid?
+  end
+  
+  test "summary should be present" do
+    @recipe.summary = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length should not be too long" do
+    @recipe.summary = "a" * 151
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length should not be too short" do
+    @recipe.summary = "aa"
+    assert_not @recipe.valid?
+  end
+  
+  test "description should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "description length should not be too long" do
+    @recipe.description = "a" * 504
+    assert_not @recipe.valid?
+  end
+  
+  test "description length should not be too short" do
+    @recipe.description = "a" * 18
+    assert_not @recipe.valid?
+  end 
+end
