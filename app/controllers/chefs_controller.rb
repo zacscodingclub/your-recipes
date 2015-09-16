d1685b04fb84b3c71de1a82431cd5f06b13d2835
zacscodingclub <zbaston@gmail.com>
2015-09-16 21:28:42 +0000

    Completed authentication

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,13 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   
   def new
     @chef = Chef.new
+  end
+  
+  def index
+    @chefs = Chef.paginate(page: params[:page], per_page: 3)
   end
   
   def create
@@ -9,6 +15,7 @@ class ChefsController < ApplicationController
     
     if @chef.save
       flash[:success] = "Your account has been created."
+      session[:chef_id] = @chef.id
       redirect_to recipes_path
     else
       render 'new'
@@ -16,22 +23,35 @@ class ChefsController < ApplicationController
   end
   
   def edit
-    @chef = Chef.find(params[:id])
+  
   end
   
   def update
-    @chef = Chef.find(params[:id])
-    
     if @chef.update(chef_params)
       flash[:success] = "Your account was updated!"
-      redirect_to recipes_path # TODO change to show chef page
+      redirect_to chef_path(@chef)
     else
       render 'edit'
     end
   end
   
+  def show
+    @recipes = @chef.recipes.paginate(page: params[:page], per_page: 2)
+  end
+  
   private
     def chef_params
       params.require(:chef).permit(:chefname, :email, :password)
+    end
+    
+    def require_same_user
+      if current_user != @chef
+        flash[:danger] = "You can only edit your own profile."
+        redirect_to root_path
+      end
+    end
+  
+    def set_chef
+      @chef = Chef.find(params[:id])
     end
 end
