2762e13d70029dad9b0b22626aa07e232ce40aa0
zacscodingclub <zbaston@gmail.com>
2015-09-18 15:10:12 +0000

    Build many to many associations with style, ingredient, and recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,10 @@
 class Recipe < ActiveRecord::Base
   belongs_to :chef
   has_many :likes
+  has_many :recipe_styles
+  has_many :styles, through: :recipe_styles
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
   validates :name, presence: true, length: { minimum: 5, maximum: 100 }
   validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
   validates :description, presence: true, length: { minimum: 20, maximum: 500 }
